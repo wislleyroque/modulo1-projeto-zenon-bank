827b7602011dd85eaf55b21b2bde593f04e7ca94
Wislley Roque <wislleyroque@hotmail.com>
2026-04-20 10:32:59 -0300

    adicionado i18n

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
@@ -1,16 +1,22 @@
 package br.com.zenon.fraud;
 
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class ReportMain
 {
     public static void main(String[] args) throws IOException {
+
         TransactionReport transactionReport = new TransactionReport();
         TransactionReport.ReportStatics rs =  transactionReport.generateTransactionReport();
 
-        System.out.println("Total de linhas: " + rs.totalLines());
-        System.out.println("Total de fraudes: " + rs.totalFrauds());
-        System.out.println("Valor total transacionado " + rs.totalAmount().toPlainString());
+        ResourceBundle bundlePT = ResourceBundle.getBundle("report", new Locale("pt", "BR"));
+        System.out.println(MessageFormat.format(bundlePT.getString("report.summary"), rs.totalLines(), rs.totalFrauds(), rs.totalAmount()));
 
+        System.out.println("---------------------------------");
+        ResourceBundle bundleEN = ResourceBundle.getBundle("report", Locale.getDefault());
+        System.out.println(MessageFormat.format(bundleEN.getString("report.summary"), rs.totalLines(), rs.totalFrauds(), rs.totalAmount()));
     }
 }
